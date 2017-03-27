965cdd646bc1b92ba5c245a792c9a0265defb0db
Savvato <d.savvato@gmail.com>
2017-03-27 21:37:02 +0300

    ## HP * writing results

--- src/main/Application.java
+++ src/main/Application.java
@@ -13,6 +13,7 @@ public class Application
 
     private double[][] coefficients = new double[NUMBER_OF_ARGUMENTS][NUMBER_OF_ARGUMENTS];
     private double[] values = new double[NUMBER_OF_ARGUMENTS];
+    private double[] arguments;
 
     private GaussMethod method;
 
@@ -25,6 +26,9 @@ public class Application
         read();
         method = new GaussMethod(coefficients, values);
         method.run();
+        arguments = method.getArguments();
+        writeArguments();
+        writeSystem();
     }
 
     private void read() throws IOException {
@@ -47,5 +51,31 @@ public class Application
         }
     }
 
+    /**
+     * Вывод аргументов уравнения
+     */
+    private void writeArguments() {
+        PrintStream writer = System.out;
+        writer.println("==============Writing arguments==============");
+        for (int i = 0; i < arguments.length; i++) {
+            writer.println("x" + Integer.toString(i + 1) + " = " + arguments[i]);
+        }
+        writer.println("============================");
+    }
 
+    /**
+     * Вывод решенной системы уравнений
+     */
+    private void writeSystem() {
+        PrintStream writer = System.out;
+        writer.println("==============System of equations==============");
+        for (int i = 0; i < coefficients.length; i++) {
+            StringBuilder stringBuilder = new StringBuilder();
+            for (int j = 0; j < coefficients[i].length; j++) {
+                stringBuilder.append(" + ").append(coefficients[i][j]).append("*").append(arguments[j]);
+            }
+            stringBuilder.append(" = ").append(values[i]);
+            writer.println(stringBuilder.toString());
+        }
+    }
 }
